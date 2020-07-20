8f412b19d7d7ed1a696c4d2d8413eb4239cc8735
ouchengle <ouchengle@gmail.com>
2020-07-20 10:16:32 +0800

    setHeaserImg

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,9 @@
     <div class="header">
         <el-col :span="8">
             <div style="display: flex;">
-                <img class="pointer" style="width: 2rem" :src="this.$store.state.user.userImg" alt="" @click="newWindow()">
+                <img class="pointer" style="width: 2rem" :src="user.userImg" alt="" @click="newWindow()">
                 <div style="display: flex;flex-direction: column;justify-content: center;margin-left: .5rem">
-                    <span>Hi, <span class="pointer" style="font-weight: bold">{{this.$store.state.user.userName}}!</span></span>
+                    <span>Hi, <span class="pointer" style="font-weight: bold">{{user.userName}}!</span></span>
                 </div>
             </div>
         </el-col>
