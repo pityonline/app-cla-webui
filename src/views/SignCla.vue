3cf8c7c11a8df415ac5234d15b4484d500091b60
ouchengle <ouchengle@gmail.com>
2020-08-19 11:24:17 +0800

    sign

--- src/views/SignCla.vue
+++ src/views/SignCla.vue
@@ -61,7 +61,7 @@
                                               :required="item.required"
                                               :prop="item.type">
                                     <el-input v-if="item.type==='email'"  type="email" v-model="ruleForm[item.type]" size="small"></el-input>
-                                    <el-input v-else-if="item.type==='date'" readonly=""  type="date" v-model="ruleForm[item.type]" size="small"></el-input>
+                                    <el-input v-else-if="item.type==='date'" readonly=""   v-model="ruleForm[item.type]" size="small"></el-input>
                                     <el-input v-else v-model="ruleForm[item.type]" size="small"></el-input>
                                 </el-form-item>
                                 <p style="font-size: .9rem;" class="borderClass">{{desc.metadataDesc}}</p>
