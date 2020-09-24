b4d7943d735b0a23dadb9789544245762cc01d45
ouchengle <ouchengle@gmail.com>
2020-09-24 15:47:08 +0800

    set minHeight

--- src/until/until.js
+++ src/until/until.js
@@ -1,8 +1,5 @@
 export const getClientHeight = () => window.innerHeight;
 export const setMinHeight = (box,inner) => {
-    this.$nextTick(() => {
         document.getElementById(box).style.minHeight = document.getElementById(inner).clientHeight * 2 + 'px'
-    })
-
 };
 
