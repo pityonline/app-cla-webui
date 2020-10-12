e3ff08bdf87ca2c8af1101485c7278bfbaf2b354
ouchengle <ouchengle@gmail.com>
2020-10-12 20:07:41 +0800

    modify create admin

--- src/views/CorporationList.vue
+++ src/views/CorporationList.vue
@@ -47,19 +47,19 @@
                         align="center">
 
                     <template slot-scope="scope">
-                        <div class="mySwitch" style="display: inline-block">
-                            <el-switch
-                                    @change="changeActive(scope.row.cla_org_id,scope.row.corporation_name,scope.row.admin_email,true)"
-                                    v-model="scope.row.enabled"
-                                    class="mySwitch"
-                                    :disabled="scope.row.enabled"
-                                    width="100"
-                                    active-color="#409EFF"
-                                    active-text="active"
-                                    inactive-text="inactive"
-                                    inactive-color="#EBEEF5">
-                            </el-switch>
-                        </div>
+                        <!--<div class="mySwitch" style="display: inline-block">-->
+                            <!--<el-switch-->
+                                    <!--@change="changeActive(scope.row.cla_org_id,scope.row.corporation_name,scope.row.admin_email,true)"-->
+                                    <!--v-model="scope.row.enabled"-->
+                                    <!--class="mySwitch"-->
+                                    <!--:disabled="scope.row.enabled"-->
+                                    <!--width="100"-->
+                                    <!--active-color="#409EFF"-->
+                                    <!--active-text="active"-->
+                                    <!--inactive-text="inactive"-->
+                                    <!--inactive-color="#EBEEF5">-->
+                            <!--</el-switch>-->
+                        <!--</div>-->
                         <el-button :disabled="scope.row.administrator_enabled" style="margin-left: 1rem" type="primary"
                                    size="mini"
                                    @click="createRoot(scope.row.admin_email)">Create Root
