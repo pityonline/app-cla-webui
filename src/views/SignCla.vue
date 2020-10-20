c86ebd13424e85f3459176f32e1536bde9f07a84
ouchengle <ouchengle@gmail.com>
2020-10-20 19:52:56 +0800

    employee sign verify email

--- src/views/SignCla.vue
+++ src/views/SignCla.vue
@@ -28,7 +28,7 @@
                                               @blur="setMyForm(item.type,ruleForm[item.id])"></el-input>
                                 </el-form-item>
                                 <el-form-item
-                                        v-if="loginType==='employee'&&rules.code"
+                                        v-if="loginType==='employee'"
                                         label="Verify Code"
                                         :required="rules.code[0].required"
                                         prop="code">
