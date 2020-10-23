d833ff96ca271341ac0f1f9cdcf7bb848fe35e81
ouchengle <ouchengle@gmail.com>
2020-10-23 10:37:19 +0800

    modify corp list

--- src/views/SignedRepo.vue
+++ src/views/SignedRepo.vue
@@ -3,7 +3,223 @@
         <CorporationHeader @clickItem="clickItem" :user="user"></CorporationHeader>
         <el-row style="padding-top: 2rem">
             <el-col :offset="4" :span="16">
-                <router-view></router-view>
+                <div>
+                    <el-tabs v-model="activeName" @tab-click="tabsHandleClick">
+                        <el-tab-pane label="Signed Corporation" name="first" style="margin-top: 1rem">
+                            <div class="tableStyle">
+                                <el-table
+                                        :data="tableData"
+                                        align="center"
+                                        style="width: 100%;">
+                                    <el-table-column
+                                            prop="corporation_name"
+                                            label="CorporationName">
+
+                                    </el-table-column>
+                                    <el-table-column
+                                            prop="admin_name"
+                                            label="AdminName">
+                                    </el-table-column>
+
+                                    <el-table-column
+                                            prop="admin_email"
+                                            label="Email">
+                                    </el-table-column>
+
+                                    <el-table-column
+                                            label="PDF"
+                                            align="center">
+
+                                        <template slot-scope="scope">
+                                            <el-popover
+                                                    width="80"
+                                                    trigger="hover"
+                                                    placement="right">
+
+                                                <div class="menuBT">
+                                                    <el-button @click="uploadClaFile(scope.row)" style="margin-left: 10px" type=""
+                                                               size="mini">upload
+                                                    </el-button>
+                                                    <el-button @click="downloadClaFile(scope.row)" type="" size="mini">download</el-button>
+                                                    <el-button @click="previewClaFile(scope.row)" type="" size="mini">preview</el-button>
+                                                </div>
+
+                                                <svg-icon slot="reference" class="pointer" icon-class="pdf" @click=""/>
+
+                                            </el-popover>
+                                        </template>
+                                    </el-table-column>
+                                    <el-table-column
+                                            align="center">
+
+                                        <template slot-scope="scope">
+                                            <!--<div class="mySwitch" style="display: inline-block">-->
+                                            <!--<el-switch-->
+                                            <!--@change="changeActive(scope.row.cla_org_id,scope.row.corporation_name,scope.row.admin_email,true)"-->
+                                            <!--v-model="scope.row.enabled"-->
+                                            <!--class="mySwitch"-->
+                                            <!--:disabled="scope.row.enabled"-->
+                                            <!--width="100"-->
+                                            <!--active-color="#409EFF"-->
+                                            <!--active-text="active"-->
+                                            <!--inactive-text="inactive"-->
+                                            <!--inactive-color="#EBEEF5">-->
+                                            <!--</el-switch>-->
+                                            <!--</div>-->
+                                            <el-button :disabled="scope.row.administrator_enabled" style="margin-left: 1rem" type="primary"
+                                                       size="mini"
+                                                       @click="createRoot(scope.row.admin_email)">Create Administrator
+                                            </el-button>
+
+                                        </template>
+                                    </el-table-column>
+
+                                </el-table>
+                            </div>
+                        </el-tab-pane>
+                        <el-tab-pane label="Bound CLA" name="second" style="margin-top: 1rem">
+                            <div class="tableStyle">
+                                <el-table
+                                        :data="tableData"
+                                        align="center"
+                                        style="width: 100%;">
+                                    <el-table-column
+                                            prop="corporation_name"
+                                            label="CorporationName">
+
+                                    </el-table-column>
+                                    <el-table-column
+                                            prop="admin_name"
+                                            label="AdminName">
+                                    </el-table-column>
+
+                                    <el-table-column
+                                            prop="admin_email"
+                                            label="Email">
+                                    </el-table-column>
+
+                                    <el-table-column
+                                            label="PDF"
+                                            align="center">
+
+                                        <template slot-scope="scope">
+                                            <el-popover
+                                                    width="80"
+                                                    trigger="hover"
+                                                    placement="right">
+
+                                                <div class="menuBT">
+                                                    <el-button @click="uploadClaFile(scope.row)" style="margin-left: 10px" type=""
+                                                               size="mini">upload
+                                                    </el-button>
+                                                    <el-button @click="downloadClaFile(scope.row)" type="" size="mini">download</el-button>
+                                                    <el-button @click="previewClaFile(scope.row)" type="" size="mini">preview</el-button>
+                                                </div>
+
+                                                <svg-icon slot="reference" class="pointer" icon-class="pdf" @click=""/>
+
+                                            </el-popover>
+                                        </template>
+                                    </el-table-column>
+                                    <el-table-column
+                                            align="center">
+
+                                        <template slot-scope="scope">
+                                            <!--<div class="mySwitch" style="display: inline-block">-->
+                                            <!--<el-switch-->
+                                            <!--@change="changeActive(scope.row.cla_org_id,scope.row.corporation_name,scope.row.admin_email,true)"-->
+                                            <!--v-model="scope.row.enabled"-->
+                                            <!--class="mySwitch"-->
+                                            <!--:disabled="scope.row.enabled"-->
+                                            <!--width="100"-->
+                                            <!--active-color="#409EFF"-->
+                                            <!--active-text="active"-->
+                                            <!--inactive-text="inactive"-->
+                                            <!--inactive-color="#EBEEF5">-->
+                                            <!--</el-switch>-->
+                                            <!--</div>-->
+                                            <el-button :disabled="scope.row.administrator_enabled" style="margin-left: 1rem" type="primary"
+                                                       size="mini"
+                                                       @click="createRoot(scope.row.admin_email)">Create Administrator
+                                            </el-button>
+
+                                        </template>
+                                    </el-table-column>
+
+                                </el-table>
+                            </div>
+                        </el-tab-pane>
+                    </el-tabs>
+
+                    <!--<div class="paginationClass">-->
+                    <!--<el-pagination-->
+                    <!--background-->
+                    <!--:page-size="5"-->
+                    <!--:pager-count="5"-->
+                    <!--:hide-on-single-page="true"-->
+                    <!--:current-page="currentPage"-->
+                    <!--@current-change="changePage"-->
+                    <!--layout="prev, pager, next"-->
+                    <!--:total="tableTotal">-->
+                    <!--</el-pagination>-->
+                    <!--</div>-->
+                    <el-dialog
+                            title="upload cla file"
+                            top="5vh"
+                            :visible.sync="uploadDialogVisible"
+                            width="35%">
+                        <div>
+
+                            <div class="left">
+                                <el-form v-model="form">
+                                    <el-form-item label="" label-width="0">
+                                        <el-upload
+                                                name="pdf"
+                                                ref="uploadPdf"
+                                                class="upload-demo"
+                                                action=""
+                                                :headers="uploadHeaders"
+                                                :http-request="upload"
+                                                :on-preview="handlePreview"
+                                                :on-remove="handleRemove"
+                                                :on-success="handleSuccess"
+                                                :before-remove="beforeRemove"
+                                                :auto-upload="false"
+                                                :on-exceed="handleExceed"
+                                                :file-list="fileList">
+                                            <el-button slot="trigger" size="small" type="primary">select </el-button>
+                                            <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">
+                                                upload
+                                            </el-button>
+                                            <!--<div slot="tip" class="el-upload__tip">文件不超过500kb</div>-->
+                                        </el-upload>
+                                    </el-form-item>
+                                </el-form>
+
+
+                            </div>
+                        </div>
+
+                    </el-dialog>
+                    <el-dialog
+                            style="background-color: #3C3C3C"
+                            title="pdf-reader"
+                            top="5vh"
+                            :close-on-click-modal="false"
+                            :visible.sync="previewDialogVisible"
+                            width="50%">
+                        <div>
+                            <pdfReader
+                                    v-if="docInfo.type === 'pdf'"
+                                    :doctype="docInfo.type"
+                                    :dochref="docInfo.href">
+
+                            </pdfReader>
+
+                        </div>
+
+                    </el-dialog>
+                </div>
             </el-col>
         </el-row>
         <el-dialog
@@ -29,7 +245,7 @@
     import * as until from '../until/until'
     import CorporationHeader from '@components/CorporationHeader'
     import Footer from '@components/NewFooter'
-
+    import pdfReader from "@components/PdfReader";
     window.onresize = () => {
         if (until.getClientHeight() > document.getElementById('signedRepo').offsetHeight) {
             document.getElementById("signedRepo").style.height = until.getClientHeight() + 'px'
@@ -40,10 +256,32 @@
         components: {
             CorporationHeader,
             Footer,
+            pdfReader,
         },
+
         data() {
             return {
-                user: this.$store.state.loginInfo,
+                activeName: 'first',
+                uploadHeaders: {
+                    'Token': this.$store.state.access_token,
+                },
+                uploadUrl: '',
+                access_token: this.$store.state.access_token,
+                refresh_token: this.$store.state.refresh_token,
+                platform: this.$store.state.platform,
+                user: {
+                    userName: this.$store.state.user.userName,
+                    userId: this.$store.state.user.userId,
+                },
+                msg: 'Welcome to Your Vue.js App',
+                docInfo: {},
+                previewDialogVisible: false,
+                form: {file: ''},
+                fileList: [],
+                uploadDialogVisible: false,
+                item: '',
+                tableData: [],
+                // user: this.$store.state.loginInfo,
 
                 active: 'first',
                 section: {
@@ -72,6 +310,124 @@
             }
         },
         methods: {
+            tabsHandleClick(tab, event) {
+                tab.index === '0' ? this.$router.push('/linkedRepo') : this.$router.push('/signedRepoLogin')
+            },
+            upload(fileObj) {
+                const formData = new FormData()
+                formData.append('pdf', fileObj.file)
+                formData.append('type', fileObj.file.type)
+                return this.$axios({
+                    url: this.uploadUrl,
+                    method: 'patch',
+                    data: formData,
+                    headers:{'Token':this.$store.state.access_token}
+                }).then()
+            },
+            uploadOk() {
+                let fd = new FormData();
+                for (let i = 0; i < this.fileList.length; i++) {
+                    fd.append('file', this.fileList[i], this.fileList[i].name);
+                }
+                this.$axios({
+                    url: this.uploadUrl,
+                    data: {fd},
+                    headers: {'Token': this.$store.state.access_token}
+                }).then(res => {
+                })
+            },
+            previewClaFile(row) {
+                this.docInfo = {
+                    type: "pdf",
+                    href:`/api${url.downloadSignature}/${this.item.id}`
+                }
+                this.previewDialogVisible = true
+            },
+
+            downloadClaFile(row) {
+                console.log('downloadClaFile', row);
+            },
+            uploadClaFile(row) {
+                this.uploadUrl = `/api${url.uploadCorporationPdf}/${this.item.id}/${row.admin_email}`
+                this.uploadDialogVisible = true
+            },
+            submitUpload() {
+                this.$refs.uploadPdf.submit();
+            },
+            handleSuccess(file, fileList) {
+                console.log(file, fileList);
+                this.fileList=[]
+                this.$message.closeAll()
+                this.$message.success('success')
+                this.uploadDialogVisible=false
+
+            },
+            handleRemove(file, fileList) {
+                console.log(file, fileList);
+            },
+            handlePreview(file) {
+                console.log(file);
+            },
+            handleExceed(files, fileList) {
+                this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+            },
+            beforeRemove(file, fileList) {
+                return this.$confirm(`Are you sure you want to remove it ${file.name}？`);
+            },
+            getCorporationInfo() {
+                http({
+                    url: `${url.corporation_signing}/${this.item.org_id}`,
+                    params: {
+                        repo_id: this.item.repo_id,
+                        cla_language: this.item.cla_language
+                    },
+                }).then(resp => {
+                    console.log(resp);
+                    this.tableData = resp.data.data[this.item.id];
+                }).catch(err => {
+                })
+            },
+            createRoot(email) {
+                this.$axios({
+                    url: `/api${url.corporationManager}/${this.item.id}/${email}`,
+                    method: 'put',
+                    headers: {
+                        'Token': this.$store.state.access_token,
+                        'Access-Token': this.access_token,
+                        'Refresh-Token': this.refresh_token,
+                        'User': `${this.platform}/${this.user.userName}`
+                    }
+                }).then(res => {
+                    this.$message.closeAll()
+                    this.$message.success('success')
+                    this.getCorporationInfo()
+                }).catch(err => {
+                    this.$message.closeAll()
+                    this.$message.error('failed')
+                })
+            },
+            changeActive(cla_org_id, corporation_name, admin_email, enabled) {
+                let data = {
+                    cla_org_id: cla_org_id,
+                    corporation_name: corporation_name,
+                    admin_email: admin_email,
+                    enabled: enabled
+                }
+                this.$axios({
+                    url: `/api${url.active_corporation}`,
+                    method: 'put',
+                    data: data,
+                    headers: {
+                        'Token': this.$store.state.access_token,
+                        'Access-Token': this.access_token,
+                        'Refresh-Token': this.refresh_token,
+                        'User': `${this.platform}/${this.user.userName}`
+                    }
+                }).then(res => {
+                    this.getCorporationInfo()
+                }).catch(err => {
+                })
+            },
             checkPdf() {
             },
             changeActive(id, active) {
@@ -139,6 +495,13 @@
             },
         },
         created() {
+            if (JSON.parse(sessionStorage.getItem('item'))) {
+                this.item = JSON.parse(sessionStorage.getItem('item'))
+            } else {
+                this.item = this.$route.query.item
+                sessionStorage.setItem('item', JSON.stringify(this.$route.query.item))
+            }
+            this.getCorporationInfo()
         },
         mounted() {
             this.setClientHeight();
