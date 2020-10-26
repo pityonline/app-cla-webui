4ad9f6f824f2f31707bdaa1bf9d52cea1ca91103
ouchengle <ouchengle@gmail.com>
2020-10-26 15:16:57 +0800

    add copy signAddress

--- src/views/LinkedRepo.vue
+++ src/views/LinkedRepo.vue
@@ -107,7 +107,7 @@
                         <template slot-scope="scope">
                             <el-button size="mini" @click="toSignPage(scope.row)">Sign</el-button>
                             <el-button size="mini" @click="copyAddress(scope.row)">copy address</el-button>
-                            <input id="signAddressInput" >
+                            <input id="signAddressInput" style="visibility: hidden;position: relative;z-index: -1">
 
                         </template>
                     </el-table-column>
