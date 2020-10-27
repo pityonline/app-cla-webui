5daaa156794c0df5eb30d8b9547452334f2fb29e
ouchengle <ouchengle@gmail.com>
2020-10-27 14:57:10 +0800

    set corp login by enter

--- src/views/CorporationManagerLogin.vue
+++ src/views/CorporationManagerLogin.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-row @keydown="submitForm('ruleForm')" style="height: 100%">
+    <el-row onkeydown="submitForm('ruleForm')" @keydown.native="submitForm('ruleForm')" style="height: 100%">
         <el-col  align="right" class="formBox">
             <div class="formBack_Box">
                 <div class="formBack">
