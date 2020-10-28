8dcd8abd85b71a9e3b913bd380fc15f916c118ac
ouchengle <ouchengle@gmail.com>
2020-10-28 11:27:25 +0800

    test email

--- src/views/SignCla.vue
+++ src/views/SignCla.vue
@@ -242,7 +242,7 @@
             },
             async verifyFormEmail(rule, value, callback) {
                 let email = value.trim();
-                let reg = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,6}$/;
+                let reg = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,6}$/;
                 if (reg.test(email)) {
                     callback();
                 } else {
