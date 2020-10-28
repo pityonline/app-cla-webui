8e04c2bd36fce0a413ae2885257bf04724b336a6
ouchengle <ouchengle@gmail.com>
2020-10-28 09:56:48 +0800

    modify signpage url

--- src/until/until.js
+++ src/until/until.js
@@ -1,7 +1,20 @@
 export const getClientHeight = () => window.innerHeight;
-export const setMinHeight = (box,inner) => {
-        document.getElementById(box).style.minHeight = document.getElementById(inner).clientHeight * 2 + 'px'
+export const setMinHeight = (box, inner) => {
+    document.getElementById(box).style.minHeight = document.getElementById(inner).clientHeight * 2 + 'px'
 };
-export const strToBase64 = (params) => window.btoa(encodeURIComponent(params));
-export const base64ToStr = (params) => window.decodeURIComponent(window.atob(params));
+export const strToBase64 = (params) => {
+    try {
+        window.btoa(encodeURIComponent(params))
+    } catch (e) {
+        return false
+    }
+};
+window.btoa(encodeURIComponent(params));
+export const base64ToStr = (params) => {
+    try {
+        window.decodeURIComponent(window.atob(params))
+    } catch (e) {
+        return false
+    }
+};
 
