a3fed9bebf0cc62865dbbb19083f7b021e41d1b1
ouchengle <969707751@qq.com>
2020-10-30 19:52:56 +0800

    modify dialog

--- src/components/CorpReLoginDialog.vue
+++ src/components/CorpReLoginDialog.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-row>
+    <el-row id="corpReLoginDialog">
         <el-dialog
                 title=""
                 :visible.sync="dialogVisible"
@@ -7,6 +7,10 @@
                 :show-close="false"
                 :close-on-click-modal="false"
                 width="30%">
+            <div class="titleBox">
+                <svg-icon icon-class="fail_icon" class="dialogIcon"></svg-icon>
+                <span>Failed</span>
+            </div>
             <el-row>
                 <el-col align="center">
                     <p>{{message}}</p>
@@ -38,16 +42,36 @@
     }
 </script>
 
-<style scoped>
-    .dialogBt {
-        margin-top: 3rem;
-        width: 8rem;
-        height: 3rem;
-        background: linear-gradient(to right, #97DB30, #319E55);
-        border-radius: 1.5rem;
-        border: none;
-        color: white;
-        cursor: pointer;
-        outline: none;
+<style lang="less">
+    #corpReLoginDialog{
+        .dialogBt {
+            margin-top: 3rem;
+            width: 8rem;
+            height: 3rem;
+            background: linear-gradient(to right, #97DB30, #319E55);
+            border-radius: 1.5rem;
+            border: none;
+            color: white;
+            cursor: pointer;
+            outline: none;
+        }
+        .el-dialog__header{
+            padding: 0;
+        }
+        .el-dialog__body{
+            padding: 20px;
+        }
+        .titleBox {
+            text-align: left;
+            font-size: 1.5rem;
+            color: #E22424;
+            margin-bottom: 1rem;
+            .dialogIcon {
+                width: 1.5rem;
+                height: 1.5rem;
+                margin-right: 1rem;
+            }
+        }
     }
+
 </style>
--- src/components/SignReLoginDialog.vue
+++ src/components/SignReLoginDialog.vue
@@ -8,8 +8,8 @@
                 :close-on-click-modal="false"
                 width="30%">
             <div class="titleBox" align="middle">
-                    <svg-icon icon-class="fail_icon" class="dialogIcon"></svg-icon>
-                    <span>Failed</span>
+                <svg-icon icon-class="fail_icon" class="dialogIcon"></svg-icon>
+                <span>Failed</span>
             </div>
             <el-row>
                 <el-col align="center">
@@ -59,8 +59,8 @@
     }
 </script>
 
-<style  lang="less">
-    #signReLoginDialog{
+<style lang="less">
+    #signReLoginDialog {
         .dialogBt {
             margin-top: 3rem;
             width: 8rem;
