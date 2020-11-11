62b93124bdafdba021b459944b34901ed9a76e11
ouchengle <969707751@qq.com>
2020-11-11 11:24:55 +0800

    change header

--- src/router/index.js
+++ src/router/index.js
@@ -156,7 +156,6 @@ router.beforeEach((to, from, next) => {
     } else {
         next()
     }
-    console.log(to);
     if (to.name === 'SignType'||to.name === 'SignType_back'||to.path === '/sign-cla'||to.path === '/index'||to.path === '/platformSelect'||to.path === '/corporationManagerLogin'||to.path === '/orgSelect') {
         sessionStorage.setItem('showHeaderMenu','false')
     }else if (to.path === '/home' || to.path === '/linkedRepo'||to.path === '/corporationList'||to.path === '/bind-cla') {
