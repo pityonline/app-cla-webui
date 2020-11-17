48cf2da8c66cd9af8858ec4f14d3071ef2884a86
ouchengle <969707751@qq.com>
2020-11-17 17:28:17 +0800

    modify get cookie

--- src/views/SignType.vue
+++ src/views/SignType.vue
@@ -620,12 +620,7 @@
                     }
                 }
             },
-            setCookie() {
-                let date = new Date();
-                date.setTime(date.getTime() - 10000);
-                let domain = this.domain.split('//')[1].split(':')[0].trim();
-                document.cookie = `_mark=; expire=${date.toUTCString()}; Domain=${domain}; path=/`;
-            },
+
             clickSignTypeGuide(type) {
                 this.signType = type;
             },
@@ -642,7 +637,6 @@
         },
         created() {
             this.setDomain();
-            this.setCookie();
             this.getRepoInfo();
         },
         mounted() {
