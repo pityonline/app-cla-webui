fb40a1a4a21f2ab3d486ce1f705d7ae81d85c730
ouchengle <969707751@qq.com>
2020-11-17 10:58:28 +0800

    modify get cookie

--- src/views/LinkedRepo.vue
+++ src/views/LinkedRepo.vue
@@ -638,7 +638,7 @@
                         }
                         let date = new Date();
                         date.setTime(date.getTime() - 10000);
-                        document.cookie = `${arr[0]}= ; expire=${date.toUTCString()}; Domain=${this.domain}; path=/`;
+                        document.cookie = `${arr[0]}=; expire=${date.toUTCString()}; Domain=${this.address}; path=/`;
                     });
                     let data = {access_token, refresh_token, platform_token,resolve};
                     this.setTokenAct(data);
