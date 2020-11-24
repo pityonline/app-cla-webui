8149b49ece1fe107dcbaa5a663885863b832f2dc
zengchen <chenzeng2@huawei.com>
2020-11-24 16:06:38 +0800

    Revert "add err tips"
    This reverts commit 4e4999c416bcf5c84977dbaea4665de1ad91e056.

--- src/views/EmployeeList.vue
+++ src/views/EmployeeList.vue
@@ -77,7 +77,7 @@
                                 :label="$t('corp.email')">
                         </el-table-column>
                         <el-table-column
-                                :label="$t('corp.operation')"
+                                label="status"
                                 align="center">
                             <template slot-scope="scope">
                                 <el-row class="mySwitch">
