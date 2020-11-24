19af888228b0ab6f546079d6edb18191f205c664
zengchen <chenzeng2@huawei.com>
2020-11-24 16:06:38 +0800

    Revert "add err tips"
    This reverts commit 4136918fe4ffb33276acbaedf201fd4b42b63468.

--- src/views/CreateManager.vue
+++ src/views/CreateManager.vue
@@ -176,7 +176,7 @@
                             case 'cla.invalid_parameter':
                                 this.$store.commit('errorCodeSet', {
                                     dialogVisible: true,
-                                    dialogMessage: this.$t('tips.manager_email_same_with_admin'),
+                                    dialogMessage: this.$t('tips.invalid_parameter'),
                                 });
                                 break;
                             case 'cla.system_error':
