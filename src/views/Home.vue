c6f4d38c41e94ce8d9fbd42a606f8e5bc1cc74d3
zengchen <chenzeng2@huawei.com>
2020-11-24 16:06:38 +0800

    Revert "modify get cookie"
    This reverts commit 211469d3dffd90a26d89d5d96b4d394462b19438.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -41,7 +41,6 @@
         methods: {
             setClientHeight() {
                 this.$nextTick(() => {
-                    document.getElementById("home").style.minHeight='0px';
                     console.log('getClientHeight===',until.getClientHeight(), '\noffsetHeight===',document.getElementById('home').offsetHeight);
                     if (until.getClientHeight() > document.getElementById('home').offsetHeight) {
                         document.getElementById("home").style.minHeight = until.getClientHeight() + 'px'
