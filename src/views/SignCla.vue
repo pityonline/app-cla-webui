4f6f7dcc784d4cffb515bb1c23214a241cfbd90b
zengchen <chenzeng2@huawei.com>
2020-11-24 16:06:38 +0800

    Revert "modify sendcode text"
    This reverts commit 728f2b34aa6f0c6d6cb38ed15df5d0457368c47a.

--- src/views/SignCla.vue
+++ src/views/SignCla.vue
@@ -33,8 +33,8 @@
                                         prop="code">
 
                                     <el-input v-model="ruleForm.code" size="small">
-                                        <el-button slot="append" :disabled="sendBtTextFromLang!==$t('signPage.sendCode')"
-                                                   @click="sendCode()">{{sendBtTextFromLang}}
+                                        <el-button slot="append" :disabled="sendBtText!==$t('signPage.sendCode')"
+                                                   @click="sendCode()">{{sendBtText}}
                                         </el-button>
                                     </el-input>
                                 </el-form-item>
@@ -131,13 +131,12 @@
             signReLoginDialogVisible() {
                 return this.$store.state.signReLoginDialogVisible
             },
-            sendBtTextFromLang: {
-                get:()=> {
-                    this.sendBtText=this.$t('signPage.sendCode')
-                    return this.sendBtText
+            sendBtText: {
+                get() {
+                    return this.$t('signPage.sendCode')
                 },
-                set:(value)=>{
-                    this.sendBtText=value
+                set(){
+                    return
                 }
             },
         },
@@ -152,7 +151,6 @@
         ,
         data() {
             return {
-                sendBtText:'',
                 signRouter: '/sign',
                 domain: this.$store.state.domain,
                 tipsTitle: '',
@@ -1181,7 +1179,6 @@
             }
 
             & > .content {
-                padding: 1rem 0;
                 text-align: left;
 
 
