1ecae2ebf7217f1ccf17aa84102d051fd1a3cf0c
zengchen <chenzeng2@huawei.com>
2020-11-24 16:06:38 +0800

    Revert "modify get cookie"
    This reverts commit d64bf0de3e6af8dfa62fe3bbd03ce1bd0a2283ec.

--- src/views/LinkedRepo.vue
+++ src/views/LinkedRepo.vue
@@ -621,8 +621,10 @@
 
             },
             getCookieData(resolve) {
-                console.log(document.cookie);
-              if (document.cookie) {
+                console.log(this.$store.state.platform_token);
+                if (this.$store.state.platform_token) {
+                    resolve('complete')
+                }else if (document.cookie) {
                     let cookieArr = document.cookie.split('; ')
                     let access_token, refresh_token, platform_token = '';
                     cookieArr.forEach((item, index) => {
@@ -641,9 +643,7 @@
                     let data = {access_token, refresh_token, platform_token,resolve};
                     this.setTokenAct(data);
 
-                }else{
-                  resolve('complete');
-              }
+                }
             },
 
             unlinkHandleClick(scope) {
