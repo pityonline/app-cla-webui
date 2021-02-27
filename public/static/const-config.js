272e4b6a96e032f7fb1d6729ca85446f5b233f93
ouchengle <969707751@qq.com>
2021-02-27 14:32:56 +0800

    change pdf max size

--- public/static/const-config.js
+++ public/static/const-config.js
@@ -1,5 +1,5 @@
 const SIGNATURE_PAGE_MAX_SIZE = 200;      //kb
-const SIGNATURE_FILE_MAX_SIZE = 3;        //M
+const SIGNATURE_FILE_MAX_SIZE = 5;        //M
 const SIGNATURE_PAGE_NAME = 'signature_page';
 const ORG_SIGNATURE_FILE = 'org_signature_file';
 const COPYRIGHT = 'Copyright ©2020 claSign';
